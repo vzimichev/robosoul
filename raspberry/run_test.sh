dcc87d4b9653001132485d42f964436e6363a6d2
Валерий Зимичев <valerii.zimichev@gmail.com>
2020-01-10 09:32:12 +0000

    no reverse

--- raspberry/run_test.sh
+++ raspberry/run_test.sh
@@ -5,10 +5,7 @@ do
  python3 executor.py -p rev &
  python3 neuro_compiler.py
  wait
- python3 RoboPy.py &
- python3 neuro_compiler.py -p rev
- wait
- python3 RoboPy.py -p rev
+ python3 RoboPy.py 
  for i in *.csv
  do
   cp -n ./$i results/${value}_${i}
