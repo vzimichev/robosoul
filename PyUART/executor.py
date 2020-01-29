885c57999eaf87f561fb5bc611d05eecd9d207d1
valerii.zimichev@gmail.com <valerii.zimichev@gmail.com>
2020-01-29 12:23:29 +0300

    minor executor fix

--- PyUART/executor.py
+++ PyUART/executor.py
@@ -2,7 +2,7 @@ import time,serial,argparse
 import numpy as np
 from RoboPy import output,fall_check,upscale_executor_matrix,normalize_sensor_data
 
-def servoin(a=90,b=90,c=90,d=90,e=90,f=90,delay=3):
+def servoin(ser,a=90,b=90,c=90,d=90,e=90,f=90,delay=3):
     '''waits end of previous act. and send angles to arduino returns acc-array     
     '''
     while ser.read()!=b'>':pass
@@ -26,28 +26,28 @@ def serial_begin(port):
     time.sleep(2)
     output(time.ctime()+'\nconnected to: '+ ser.portstr,'highlight')
     ser.write('in5a5a5a5a5a5a03'.encode())
-    return ser,np.array([[90,90,90,90,90,90]])
+    return ser
 
-def executor(ser,prt,mtrx):
+def executor(ser,mtrx):
     k = 0
     acc = []
     try:#fall detection script            
         while k == 0:
             for i in mtrx: 
-                acc.append(servoin(*i))
+                acc.append(servoin(ser,*i))
                 k += 1
     except ZeroDivisionError:
         output('I have fallen.\n','error')
-        output('Steps to fall:'+str(stf)+'\n','highlight')
         if len(acc) != 0: 
             sensor_data = normalize_sensor_data(np.array(acc))
             np.savetxt('sensor.csv',sensor_data,fmt='%.4f',delimiter=',')
             output('[Upd]sensor.csv\nSensor data recieved.\n')   
         else: output('No sensor data recieved.\n','warning') 
-        stand_up([90,90,90,90,90,90])
-    return ser,k,acc
+        output('Steps to fall:'+str(k)+'\n','highlight')
+        stand_up()
 
-def stand_up(old):
+
+def stand_up(old=[90,90,90,90,90,90]):
     """TRUE when ready"""
     try:
         data='in'
@@ -73,12 +73,12 @@ if __name__ == "__main__":
     if prefix != '': prefix = prefix + '_'    
     
     port = '/dev/ttyACM0'
-    ser,matrix = serial_begin(port)
+    ser = serial_begin(port)
     matrix = np.loadtxt('matrix.csv', 'float', delimiter = ',')
     restrictions = np.loadtxt('restrictions.txt', 'int', delimiter = '\t')       
     matrix = upscale_executor_matrix(matrix,restrictions)
     
-    ser,stf,sensor_data = executor(ser,port,matrix.astype(int))
+    executor(ser,matrix.astype(int))
     
     ser.close()
     output('Session of executor.py ended in ','time',time.time()-start_time)  
