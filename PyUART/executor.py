8625c8537e6136402b19a6fa0d24a6387a77b60d
valerii.zimichev@gmail.com <valerii.zimichev@gmail.com>
2020-01-30 09:55:25 +0300

    success case fix

--- PyUART/executor.py
+++ PyUART/executor.py
@@ -15,8 +15,8 @@ def servoin(ser,a=90,b=90,c=90,d=90,e=90,f=90,delay=3):
             data+=str(hex(i))[2]+str(hex(i))[3]
         except IndexError:
             data+='0'+str(hex(i))[2]
-        ser.write(data.encode())
-        output('servo in: '+str([a,b,c,d,e,f,delay])+'\n')       
+    ser.write(data.encode())
+    output('servo in: '+str([a,b,c,d,e,f,delay])+'\n')       
     
 def serial_begin(port):
     ser = serial.Serial(port, 19200, bytesize=8, parity='N', stopbits=1, timeout=2)
@@ -45,6 +45,13 @@ def executor(ser,mtrx):
             else: output('No sensor data recieved.\n','warning') 
             output('Steps to fall:'+str(k)+'\n','highlight')
             return False
+    output('All matrix have been executed.\n')
+    if len(acc) != 0: 
+        sensor_data = normalize_sensor_data(np.array(acc))
+        np.savetxt('sensor.csv',sensor_data,fmt='%.4f',delimiter=',')
+        output('[Upd]sensor.csv\nSensor data recieved.\n')   
+    else: output('No sensor data recieved.\n','warning') 
+    output('Steps to fall:'+str(k)+'\n','highlight')
     return True
  
 if __name__ == "__main__":
