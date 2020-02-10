c5073c455d75f57861915d74fbb7015ec52638e1
valerii.zimichev@gmail.com <valerii.zimichev@gmail.com>
2020-02-10 07:41:51 +0300

    parallel config fix

--- PyUART/coordinator.py
+++ PyUART/coordinator.py
@@ -1,32 +1,30 @@
 from threading import Thread
 from RoboPy import output
-import os
+from os import system
 
 if __name__ == "__main__":
     for j in range(1,26):        
+        output('Launch #'+str(j))
+               
         #forward
-        executor = Thread(target = os.system, args = ('python3 executor.py', ))
-        neuro_compiler = Thread(target = os.system, args = ('python3 neuro_compiler.py -p pre', ))
-        RoboPy = Thread(target = os.system, args = ('python3 RoboPy.py -p pre', ))
+        executor = Thread(target = system, args = ('python3 executor.py', ))
+        neuro_compiler = Thread(target = system, args = ('python3 neuro_compiler.py -p pre', ))
+        Robo_Py = Thread(target = system, args = ('python3 RoboPy.py -p pre', ))
         #reverse
-        rev_neuro_compiler = Thread(target = os.system, args = ('python3 neuro_compiler.py -p rev', ))
-        rev_RoboPy = Thread(target = os.system, args = ('python3 RoboPy.py -p rev', ))
+        rev_neuro_compiler = Thread(target = system, args = ('python3 neuro_compiler.py -p rev', ))
+        rev_RoboPy = Thread(target = system, args = ('python3 RoboPy.py -p rev', ))
         #iteration
-        it_neuro_compiler = Thread(target = os.system, args = ('python3 neuro_compiler.py -c True -p rev', ))
-        #output
-        print_output = Thread(target = output, args = ('Launch #'+str(j),))
-                                                       
-        print_output.start()
-        print_output.join()
+        it_neuro_compiler = Thread(target = system, args = ('python3 neuro_compiler.py -c True -p rev', ))
+        
         executor.start()
         neuro_compiler.start()
-        executor.join()
         neuro_compiler.join()
-        RoboPy.start()
+        executor.join()
+        Robo_Py.start()
+        Robo_Py.join()
         rev_neuro_compiler.start()
         rev_neuro_compiler.join()
         rev_RoboPy.start()
         rev_RoboPy.join()
         it_neuro_compiler.start()
         it_neuro_compiler.join()
-        RoboPy.join()
