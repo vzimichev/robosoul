97db0bbbe5d4e016faa4bcf15e04ecc285822f2a
Валерий Зимичев <valerii.zimichev@gmail.com>
2020-02-14 09:18:40 +0000

    sarge import

--- PyUART/coordinator.py
+++ PyUART/coordinator.py
@@ -1,11 +1,13 @@
 from threading import Thread
 from RoboPy import output
-from os import system
+from sarge import run as system
+
 
 if __name__ == "__main__":
+
     for j in range(1,26):        
-        output('Launch #'+str(j))
-               
+        system('echo "Launch #'+str(j)+'"')
+        
         #forward
         executor = Thread(target = system, args = ('python3 executor.py', ))
         neuro_compiler = Thread(target = system, args = ('python3 neuro_compiler.py -p pre', ))
@@ -17,14 +19,25 @@ if __name__ == "__main__":
         it_neuro_compiler = Thread(target = system, args = ('python3 neuro_compiler.py -c True -p rev', ))
         
         executor.start()
+
         neuro_compiler.start()
+
         neuro_compiler.join()
+
         executor.join()
+
         Robo_Py.start()
+
         Robo_Py.join()
+
         rev_neuro_compiler.start()
+
         rev_neuro_compiler.join()
+
         rev_RoboPy.start()
+
         rev_RoboPy.join()
+
         it_neuro_compiler.start()
+
         it_neuro_compiler.join()
