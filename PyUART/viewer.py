d78270f2d8ef158a585639d994e9ad17bf93dab6
Валерий Зимичев <valerii.zimichev@gmail.com>
2020-02-18 12:57:48 +0000

    excel graph

--- PyUART/viewer.py
+++ PyUART/viewer.py
@@ -1,8 +1,12 @@
 import numpy as np
 import time
 import json
+import openpyxl
 import pandas as pd
 from RoboPy import output
+from openpyxl.chart import LineChart,Reference,Series
+from openpyxl import load_workbook
+
 
 if __name__ == "__main__":
     start_time = time.time()
@@ -17,12 +21,52 @@ if __name__ == "__main__":
             data.update({i['prefix'] :   [*[i[k] for k in params],*list(np.loadtxt(i['report'], 'int', delimiter=',')[:,0])]})
             target.update({i['prefix'] : [*[i[k] for k in params],*list(np.loadtxt(i['report'], 'float', delimiter=',')[:,1])]})
     index = [*params,*[i for i in range(1,len(data['executor'])-len(params)+1)]]
+
     df = pd.DataFrame(data,index=index)
     df.to_excel('steps_ev.xlsx')
+    print('Steps to fall\n',df)
     tf = pd.DataFrame(target, index=index)
     tf.to_excel('target_ev.xlsx')
-    
-    print('Steps to fall\n',df)
     print('Target function\n',tf)
+    
+    report = load_workbook(filename="target_ev.xlsx")     
+    sheet = report.active 
+    chart = LineChart()
+    k = 2
+    for i in target:
+        values = Reference(sheet, min_col = k, min_row = len(params) + 2, max_row = sheet.max_row)
+        series = Series(values , title = i)
+        chart.series.append(series)
+        k += 1
+    chart.title = " Absolute value of target function (prediction)"
+    chart.x_axis.title = " Iteration "
+    chart.y_axis.title = " Target function "
+    sheet.add_chart(chart, "H2") 
+    report.save("target_ev.xlsx")
+
+    
+    rev_target = {}
+    for i in CONFIG: 
+        if i['target'] == 'reverse':
+            rev_target.update({i['prefix'] : [*[i[k] for k in params],*list(np.loadtxt(i['report'], 'float', delimiter=','))]})
+   
+    tf = pd.DataFrame(rev_target, index=index)
+    tf.to_excel('reverse_target_ev.xlsx')
+    print('Target function\n',tf)
+    
+    report = load_workbook(filename="reverse_target_ev.xlsx")     
+    sheet = report.active 
+    chart = LineChart()
+    k = 2
+    for i in target:
+        values = Reference(sheet, min_col = k, min_row = len(params) + 2, max_row = sheet.max_row)
+        series = Series(values , title = i)
+        chart.series.append(series)
+        k += 1
+    chart.title = " Absolute value of target function (reverse)"
+    chart.x_axis.title = " Iteration "
+    chart.y_axis.title = " Target function "
+    sheet.add_chart(chart, "H2") 
+    report.save("reverse_target_ev.xlsx")    
     
     output('Session of viewer.py ended in ','time',time.time()-start_time)  
