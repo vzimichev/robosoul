58ff1dec32cfe2b88ccd341adc11402ace745a4f
Валерий Зимичев <valerii.zimichev@gmail.com>
2020-02-28 06:00:50 +0000

    10x5 series

--- raspberry/run_test.sh
+++ raspberry/run_test.sh
@@ -2,18 +2,21 @@
 set -e
 echo "Launch #0. (executor only)"
 python3 executor.py
-
-for value in $(seq 25)
+for series in $(seq 10)
 do
- echo "Launch #${value}."
- python3 executor.py &
- chmod -c 444 sensor.csv
- python3 neuro_compiler.py -t prediction 
- python3 RoboPy.py -t prediction
- python3 neuro_compiler.py -p rev
- python3 RoboPy.py -p rev --learning 0.05 --hyper 0.03 --cool 0.1
- python3 neuro_compiler.py -c True -p rev
- chmod -c 644 sensor.csv
- wait
+    for value in $(seq 5)
+    do
+     echo "Launch #${value}. Series #${series}."
+     python3 executor.py &
+     chmod -c 444 sensor.csv
+     python3 neuro_compiler.py -t prediction 
+     python3 RoboPy.py -t prediction
+     python3 neuro_compiler.py -p rev
+     python3 RoboPy.py -p rev --learning 0.05 --hyper 0.03 --cool 0.1
+     python3 neuro_compiler.py -c True -p rev
+     chmod -c 644 sensor.csv
+     wait
+    done
+    python3 matrix_inpreter.py -r False
 done
 
