bfd30eea5576b6234691e250266847432965c666
valerii.zimichev <valerii.zimichev@gmail.com>
2020-02-29 11:30:56 +0300

    single report

--- PyUART/viewer.py
+++ PyUART/viewer.py
@@ -7,10 +7,11 @@ from openpyxl import load_workbook
 if __name__ == "__main__":
     start_time = time.time()
     params = ['strategy','foresight','learning rate','hyper','cool']
+    report_filename = 'report_ev.xlsx'
     executor_filename = 'executor_report.xls'
-    steps_filename = 'steps_ev.xlsx'
-    loss_filename = 'loss_ev.xlsx'
-    rev_loss_filename = 'reverse_loss_ev.xlsx'
+    steps_sheetname = 'steps'
+    loss_sheetname = 'loss'
+    rev_loss_sheetname = 'reverse_loss'
     data = {'executor': [*['' for k in params],*[i for i in list(np.loadtxt(executor_filename, 'int'))][:-1]]}
     loss = {}
 
@@ -20,58 +21,54 @@ if __name__ == "__main__":
             data.update({i['prefix'] :   [*[i[k] for k in params],*list(np.loadtxt(i['report'], 'int', delimiter=',')[:,0])]})
             loss.update({i['prefix'] : [*[i[k] for k in params],*list(np.loadtxt(i['report'], 'float', delimiter=',')[:,1])]})
     index = [*params,*[i for i in range(1,len(data['executor'])-len(params)+1)]]
-
-    df = pd.DataFrame(data,index=index)
-    df.to_excel(steps_filename)
-    output('[Upd]'+steps_filename)
-    print('Steps to fall\n',df)
-    tf = pd.DataFrame(loss, index=index)
-    tf.to_excel(loss_filename)
-    output('[Upd]'+loss_filename)
-    print('Loss function\n',tf)
-    
-    report = load_workbook(filename=loss_filename)     
-    sheet = report.active 
-    chart = LineChart()
-    k = 2
-    for i in loss:
-        values = Reference(sheet, min_col = k, min_row = len(params) + 2, max_row = sheet.max_row)
-        series = Series(values , title = i)
-        chart.series.append(series)
-        k += 1
-    chart.title = " Absolute value of loss function (prediction)"
-    chart.x_axis.title = " Iteration "
-    chart.y_axis.title = " Loss function "
-    sheet.add_chart(chart, "H2") 
-    report.save(loss_filename)
-    output('[Upd]'+loss_filename)
-    output('Charts plotted.')
     
     rev_loss = {}
     for i in CONFIG: 
         if i['target'] == 'reverse':
             rev_loss.update({i['prefix'] : [*[i[k] for k in params],*list(np.loadtxt(i['report'], 'float', delimiter=','))]})
    
-    tf = pd.DataFrame(rev_loss, index=index)
-    tf.to_excel(rev_loss_filename)
-    output('[Upd]'+rev_loss_filename)
-    print('Loss function\n',tf)
-    
-    report = load_workbook(filename=rev_loss_filename)     
-    sheet = report.active 
+    with pd.ExcelWriter(report_filename) as writer:  
+        df = pd.DataFrame(data,index=index)
+        df.to_excel(writer,sheet_name = steps_sheetname)
+        tf = pd.DataFrame(loss, index=index)
+        tf.to_excel(writer,sheet_name = loss_sheetname)
+        rf = pd.DataFrame(rev_loss, index=index)
+        rf.to_excel(writer,sheet_name=rev_loss_sheetname)
+        output('[Upd]'+report_filename)
+        
+    print('Steps to fall\n',df)
+    print('Loss function for prediction\n',tf)
+    print('Loss function for reverse\n',rf)
+    #>>>charts   
+    report = load_workbook(filename=report_filename)
+    sheet = report.active
+    sheet1 = report.get_sheet_by_name(loss_sheetname)
     chart = LineChart()
     k = 2
     for i in loss:
-        values = Reference(sheet, min_col = k, min_row = len(params) + 2, max_row = sheet.max_row)
+        values = Reference(sheet1, min_col = k, min_row = len(params) + 2, max_row = sheet.max_row)
+        series = Series(values , title = i)
+        chart.series.append(series)
+        k += 1
+    chart.title = " Absolute value of loss function (prediction)"
+    chart.x_axis.title = " Iteration "
+    chart.y_axis.title = " Loss function "
+    sheet1.add_chart(chart, "K2") 
+   
+    sheet2 = report.get_sheet_by_name(rev_loss_sheetname)
+    chart = LineChart()
+    k = 2
+    for i in rev_loss:
+        values = Reference(sheet2, min_col = k, min_row = len(params) + 2, max_row = sheet.max_row)
         series = Series(values , title = i)
         chart.series.append(series)
         k += 1
     chart.title = " Absolute value of loss function (reverse)"
     chart.x_axis.title = " Iteration "
     chart.y_axis.title = " Loss function "
-    sheet.add_chart(chart, "H2") 
-    report.save(rev_loss_filename) 
-    output('[Upd]'+rev_loss_filename)
+    sheet2.add_chart(chart, "H2") 
+    report.save(report_filename) 
+    output('[Upd]'+report_filename)
     output('Charts plotted.')
-    
+
     output('Session of viewer.py ended in ','time',time.time()-start_time)  
