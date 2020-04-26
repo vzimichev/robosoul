ec459d0ba4268070a3185390b2ff7183c9d7472c
valerii.zimichev@gmail.com <valerii.zimichev@gmail.com>
2020-04-26 16:44:30 +0300

    catch input exceptions - arduino

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -72,21 +72,23 @@ void setup()
 
 void loop()
 {
-
-    if (Serial.find("in"))
-    {
+  if (Serial.find("in")) {
       data = Serial.readString();
-      for (byte i = 0; i < 7; i++){
+      if (data.length() != 15) return;
+      for (byte i = 0; i<16; i++)  {
+        if (static_cast<int>(data[i]) > 103) return;
+      }
+      for (byte i = 0; i < 7; i++) {
         ang[i] = strtol(&data.substring(2*i, 2*i+2)[0],NULL,16)+initial[i];        //getting ang[]
-}//for reading
-
-//movement from last[] to ang[]
-     while(pos[0] != ang[0] || pos[1] != ang[1] || pos[2] != ang[2] || pos[3] != ang[3] || pos[4] != ang[4] || pos[5] != ang[5]){
-      for (byte i = 0; i<6;i++){
+      }//for reading
+  
+//moving from last[] to ang[]
+      while(pos[0] != ang[0] || pos[1] != ang[1] || pos[2] != ang[2] || pos[3] != ang[3] || pos[4] != ang[4] || pos[5] != ang[5]) {
+      for (byte i = 0; i<6;i++) {
           if (last[i] == ang[i]) continue;
           if (pos[i] == ang[i]) continue;
           int c = 100*(pos[i]-ang[i])/abs(last[i]-ang[i]);
-          if (c > 0){
+          if (c > 0) {
             if ((25 < c) && (c < 75) && (c != 50)) pos[i]-=2;
             else pos[i]-=1;
           }
@@ -96,22 +98,21 @@ void loop()
           }
           servo[i].write(pos[i]);
           delay(ang[6]);
- }//for index      
- }//while stop
-  for (byte i = 0; i < 6; i++) last[i]=pos[i];
-  sensors_event_t event; 
-  accel.getEvent(&event);
-  
-  int gxRaw, gyRaw, gzRaw;
-  float gx, gy, gz;  
-  BMI160.readGyro(gxRaw, gyRaw, gzRaw);
-  gx = convertRawGyro(gxRaw);
-  gy = convertRawGyro(gyRaw);
-  gz = convertRawGyro(gzRaw);
-  
-  Serial.print(">"); Serial.print(event.acceleration.x); Serial.print("\t");Serial.print(event.acceleration.y); Serial.print("\t");Serial.print(event.acceleration.z);Serial.print("\t");Serial.print(gx); Serial.print("\t");Serial.print(gy); Serial.print("\t");Serial.println(gz);
-
- //Serial.println("end");
- }//if IN
-
- }//void
+      }//for index      
+      }//while stop
+      
+//getting sensor data      
+      for (byte i = 0; i < 6; i++) last[i]=pos[i];
+      sensors_event_t event; 
+      accel.getEvent(&event);
+      
+      int gxRaw, gyRaw, gzRaw;
+      float gx, gy, gz;  
+      BMI160.readGyro(gxRaw, gyRaw, gzRaw);
+      gx = convertRawGyro(gxRaw);
+      gy = convertRawGyro(gyRaw);
+      gz = convertRawGyro(gzRaw);
+      
+      Serial.print(">"); Serial.print(event.acceleration.x); Serial.print("\t");Serial.print(event.acceleration.y); Serial.print("\t");Serial.print(event.acceleration.z);Serial.print("\t");Serial.print(gx); Serial.print("\t");Serial.print(gy); Serial.print("\t");Serial.println(gz);
+      }//if IN
+}//void
