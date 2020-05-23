aef71898d64f6c755aec90202c706c7199a38827
vzimichev <valerii.zimichev@gmail.com>
2020-05-23 18:33:54 +0300

    refactoring

--- scripts/build_initials.sh
+++ scripts/build_initials.sh
@@ -6,7 +6,7 @@ echo "CI_FORESIGHT: $CI_FORESIGHT"
 echo "CI_HYPER: $CI_HYPER"
 echo "CI_COOLDOWN: $CI_COOLDOWN"
 chmod -R 777 * >chmod.log
-cp PyUART/* .
+cp src/* .
 python matrix_inpreter.py walk
 python weight_matrix_creator.py --target prediction --strategy $CI_STRATEGY --foresight $CI_FORESIGHT --prefix pre3 --learning 0.3   --hyper $CI_HYPER --cool $CI_COOLDOWN
 python weight_matrix_creator.py --target prediction --strategy $CI_STRATEGY --foresight $CI_FORESIGHT --prefix pre4 --learning 0.1   --hyper $CI_HYPER --cool $CI_COOLDOWN
