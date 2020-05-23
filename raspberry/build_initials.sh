eb99e6fb7c44187727429dbbc75182a257c1e672
Валерий Зимичев <valerii.zimichev@gmail.com>
2020-05-23 15:14:47 +0000

    config upload

--- raspberry/build_initials.sh
+++ raspberry/build_initials.sh
@@ -13,4 +13,4 @@ python weight_matrix_creator.py --target prediction --strategy $CI_STRATEGY --fo
 python weight_matrix_creator.py --target prediction --strategy $CI_STRATEGY --foresight $CI_FORESIGHT --prefix pre5 --learning 0.05  --hyper $CI_HYPER --cool $CI_COOLDOWN
 python weight_matrix_creator.py --target prediction --strategy $CI_STRATEGY --foresight $CI_FORESIGHT --prefix pre6 --learning 0.01  --hyper $CI_HYPER --cool $CI_COOLDOWN
 python weight_matrix_creator.py --target prediction --strategy $CI_STRATEGY --foresight $CI_FORESIGHT --prefix pre7 --learning 0.005 --hyper $CI_HYPER --cool $CI_COOLDOWN
-python weight_matrix_creator.py --target reverse --prefix rev --strategy 666 --foresight 8
+python weight_matrix_creator.py --target reverse --prefix rev --strategy 667 --foresight 8
